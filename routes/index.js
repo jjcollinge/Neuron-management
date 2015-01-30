956a07446e57b616b96cbd2de05c77442ad5b42a
jjcollinge <jonathancollinge@live.com>
2015-01-30 22:56:45 +0000

    Basic jade templating

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,9 @@ router.get('/', function(req, res, next) {
   request("http://localhost:9998/api/devices/", function(error, response, body) {
   	if(!error && response.statusCode == 200) {
   		var deviceArray = JSON.parse(body);
-  		res.render('index', { title: 'Express', devices: JSON.stringify(deviceArray) });
+  		res.render('index', { title: 'Express', devices: deviceArray });
   	}
   })
 });
-
 
 module.exports = router;
